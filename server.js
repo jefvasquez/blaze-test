cc9672e3f9311f9fc9f332f7dc747a55991d1e6d
Jeferson Vasquez <jefvasquez@icloud.com>
2020-09-03 17:36:39 -0500

    editing server.js

--- server.js
+++ server.js
@@ -1,23 +1,22 @@
 const express = require('express');
 const app = express();
 
+const routes = require('./routes');
+
 const PORT = process.env.PORT || 5000;
+
+// require db connection
+require('./models');
 
 // configure body parser for AJAX requests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// routes
-app.get('/', (req, res) => {
-	res.send('Hello from MERN');
-});
+// ADD THIS LINE
+app.use(express.static('frontend/build'));
 
-if(process.env.NODE_ENV === "production") {
-    app.use(express.static("frontend/build"));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
-    });
-}
+app.use(routes);
+
 // Bootstrap server
 app.listen(PORT, () => {
 	console.log(`Server listening on port ${PORT}.`);
