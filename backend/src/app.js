a45b42e8cc068a790c3e6711886aa0befa5fb685
Jeferson Vasquez <jefvasquez@icloud.com>
2020-09-03 17:07:05 -0500

    editing server.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,11 +9,11 @@ app.use(express.json());
 
 app.use('/api/customers',require('./routes/customers'));
 
-if(process.env.NODE_ENV === "production") {
-    app.use(express.static("frontend/build"));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
-    });
-}
+// if(process.env.NODE_ENV === "production") {
+//     app.use(express.static("frontend/build"));
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
+//     });
+// }
 
 module.exports = app;
