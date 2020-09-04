6554645229bca53b67594401fbd1b735a3dc9ef9
Jeferson Vasquez <jefvasquez@icloud.com>
2020-09-03 21:48:10 -0500

    deleting server.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,19 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 
 app.set('port',process.env.PORT || 4500);
 
 app.use(cors());
-app.use(express.json());
+app.use(bodyParser.json());
 
 app.use('/api/customers',require('./routes/customers'));
 
-if(process.env.NODE_ENV === "production") {
+// if(process.env.NODE_ENV === "production") {
     app.use(express.static("frontend/build"));
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
     });
-}
+// }
 
 module.exports = app;
